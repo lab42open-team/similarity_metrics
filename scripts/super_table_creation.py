1a1074c1f1e125057491afaeb30c39ed0d49c450
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-03-15 16:29:23 +0200

    super table test script

--- scripts/super_table_creation.py
+++ scripts/super_table_creation.py
@@ -1,55 +1,73 @@
-import os, sys, re
-import numpy as np
-from scipy.spatial import distance
+import os, sys 
 
-def merged_data(directory):
-    merged_data = {}
+def merge_data(input_dir):
     sample_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(".tsv"):
-            filepath = os.path.join(directory, filename)
-            with open(filepath, "r") as file:
-                lines = file.readlines()
-                header = lines[0].strip().split("\t")[1:]
+    taxa_counts = {}
+    all_taxa = set()
+    
+    # Iterate through all files in parent directory
+    for file in os.listdir(input_dir):
+        if file.endswith(".tsv"):
+            target_file = os.path.join(input_dir, file)
+            with open(target_file, "r") as file:
+                print("processing file: ", target_file)
+                # Extract sample named from header
+                header = next(file).strip().split("\t")[1:]
+                #print("Header", header)
                 sample_names.extend(header)
-                # Iterate for each line in file
-                for line in lines[1:]:
-                    # Separate columns by tab 
+                #print("Sample names", sample_names)
+                
+                # Initialize taxa counts dictionary 
+                for sample_name in sample_names:
+                    taxa_counts[sample_name] = {}
+                    
+                # Process each line in the file 
+                for line in file:
                     columns = line.strip().split("\t")
-                    # Retrieve taxa from first column
+                    #print(columns)
                     taxa = columns[0]
-                    # Retrieve values from the rest columns (except 1st)
-                    values = list(columns[1:]) ### TODO check test_similarity_metrics.py for dict
-                    if len(values) < len(sample_names):
-                        values.extend([0] * (len(sample_names) - len(values))) ### TODO correction needed
-                    merged_data.setdefault(taxa, []).extend(values)
-    return sample_names, merged_data
+                    #print(taxa)
+                    counts = columns[1:]
+                    # Update set of all taxa
+                    all_taxa.add(taxa)
+                    
+                    # Update taxa counts dictionary 
+                    for i, sample_name in enumerate(sample_names):
+                        count = counts[i] if i < len(counts) else "0"
+                        try:
+                            count = float(count)
+                        except ValueError:
+                            count = 0
+                        taxa_counts[sample_name][taxa] = count
+                print(sample_name, taxa, columns)        
+    # Fill missing taxa counts with zeros
+    for sample_name in sample_names: 
+        for taxa in all_taxa:
+            if taxa not in taxa_counts[sample_name]:
+                taxa_counts[sample_name][taxa] = 0
+                    
+    return sample_names, taxa_counts
 
-def write_output(sample_names, merged_data, output_file):
+# TODO: print is correct, but output not. Needs correction 
+def write_output(sample_names, taxa_counts, output_file):
+    sample_names, taxa_counts = merge_data
     with open(output_file, "w") as file:
+        # Write header
         file.write("Taxa\t" + "\t".join(sample_names) + "\n")
-        for taxa, values in merged_data.items():
-            file.write(taxa +"\t" + "\t".join((map(str, values))) + "\n")
-               
+        # Write taxa counts for each sample
+        for taxa, count_dict in taxa_counts.items():
+            file.write(taxa + "\t")
+            for sample_name in sample_names: 
+                count = count_dict.get(sample_name, 0)
+                file.write("\t{}".format(count))
+            file.write("\n")
+        
+            
 def main():
-    # Set input directory
-    #input_ssu_directory = "/ccmri/similarity_metrics/data/taxa_counts_output/v5_SSU"
-    #input_lsu_directory = "/ccmri/similarity_metrics/data/taxa_counts_output/v5_LSU"
-    input_test_directory = "/ccmri/similarity_metrics/data/test_dataset"
-    # Set output directory
-    #output_ssu_file_path = "/ccmri/similarity_metrics/data/version_5.0/SSU/SSU_v5_super_table.tsv"
-    #output_lsu_file_path = "/ccmri/similarity_metrics/data/version_5.0/LSU/LSU_v5_super_table.tsv"                    
-    output_test_directory = "/ccmri/similarity_metrics/data/test_dataset/output.tsv"
-    
-    """sample_names_ssu, merged_data_ssu = merged_data(input_ssu_directory)
-    write_output(sample_names_ssu, merged_data_ssu)
-    
-    sample_names_lsu, merged_data_lsu = merged_data(input_lsu_directory)
-    write_output(sample_names_lsu, merged_data_lsu)      """
-    # TODO : correct script, there are some wrong values + gaps 
-    sample_names_test, merged_data_test = merged_data(input_test_directory)
-    write_output(sample_names_test, merged_data_test, output_test_directory) 
+    input_dir = "/ccmri/similarity_metrics/data/test_dataset"
+    output_file = "/ccmri/similarity_metrics/data/test_dataset/output.tsv"
+    sample_names_test, taxa_counts_test = merge_data(input_dir)
+    write_output(sample_names_test, taxa_counts_test, output_file)
 
 if __name__ == "__main__":
-    main()
-    
+    main()       
