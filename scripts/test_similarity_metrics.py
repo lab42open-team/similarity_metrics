2785f3271e6298967b3f98212237e9980e722aa1
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-03-28 13:39:07 +0200

    jaccard testing

--- scripts/test_similarity_metrics.py
+++ scripts/test_similarity_metrics.py
@@ -17,33 +17,62 @@ from scipy.stats import spearmanr
 #input_dir = "/ccmri/profile_matching/test_dataset/MGYS_taxa_counts_output" # test
 # Read target file 
 #input_file = "/ccmri/data_similarity_metrics/test_dataset/MGYS_taxa_counts_output/MGYS00000305_ERP001568_taxonomy_abundances_v1.0.tsv_output.tsv" #[file for file in os.listdir(input_dir)]
-input_file = "/ccmri/data_similarity_metrics/test_dataset/MGYS00006566_SRP345292_taxonomy_abundances_LSU_v5.0.tsv_output.tsv" #[file for file in os.listdir(input_dir)]
+#input_file = "/ccmri/similarity_metrics/data/test_dataset/output.tsv" #[file for file in os.listdir(input_dir)]
 
-# Dictionary to store counts for each sample
-sample_counts = {}
-with open(input_file, "r") as file:
-    # Read header & sample IDs
-    header = file.readline().strip().split("\t")
-    sample_ids = header[1:]
-    # Initialize sample counts dictionary
-    for sample_id in sample_ids:
-        sample_counts[sample_id] = {}
-    # Read counts for each taxon 
-    for line in file:
-        columns = line.strip().split("\t")
-        taxon = columns[0]
-        counts = list(map(float, columns[1:]))
-        for i, sample_id in enumerate(sample_ids):
-            sample_counts[sample_id][taxon] = counts[i]
-        #print(header, taxon, counts)
-
-# Iterate over indices of Sample IDs
-for i in range(len(sample_ids)):
+def jaccard_score(input_file):
+    # Dictionary to store counts for each sample
+    sample_counts = {}
+    with open(input_file, "r") as file:
+        # Read header & sample IDs
+        header = file.readline().strip().split("\t")
+        sample_ids = header[1:]
+        # Initialize sample counts dictionary
+        for sample_id in sample_ids:
+            sample_counts[sample_id] = {}
+        # Read counts for each taxon 
+        for line in file:
+            columns = line.strip().split("\t")
+            taxon = columns[0]
+            counts = list(map(float, columns[1:]))
+            for i, sample_id in enumerate(sample_ids):
+                sample_counts[sample_id][taxon] = counts[i]
+            #print(header, taxon, counts)
+        # Initialize jaccard_score 
+        jaccard_scores = []    
+    # Iterate over indices of Sample IDs
+    for i in range(len(sample_ids)):
     # Iterate over indices of sample IDs starting from i+1 to avoid comparing a sample to itself (duplicates)
-    for j in range(i + 1, len(sample_ids)):
-        # Define sets of ith and jth sample
-        set1 = np.array(list(sample_counts[sample_ids[i]].values()))
-        set2 = np.array(list(sample_counts[sample_ids[j]].values()))
+        for j in range(i + 1, len(sample_ids)):
+            # Define sets of ith and jth sample
+            set1 = np.array(list(sample_counts[sample_ids[i]].values()))
+            set2 = np.array(list(sample_counts[sample_ids[j]].values()))
+            jaccard_distance = distance.jaccard(set1, set2) 
+            jaccard_scores.append((sample_ids[i], sample_ids[j], jaccard_distance))
+            #print("Jaccard Similarity Score between {}, {} = {}.".format(sample_ids[i], sample_ids[j], jaccard_distance))
+
+    return jaccard_scores
+                
+#jaccard = jaccard_score(input_file)                
+
+def write_output(jaccard_scores, output_file):        
+    with open(output_file, "w") as file:
+        file.write("Sample i\tSample j\tJaccard Score\n")
+        for item in jaccard_scores:
+            file.write("{}\t{}\t{}\n".format(item[0], item[1], item[2]))
+            
+def main():
+    input_file = "/ccmri/similarity_metrics/data/test_dataset/output.tsv"
+    output_file = "/ccmri/similarity_metrics/data/test_dataset/jaccard_output.tsv"
+    test_jaccard_smlrt = jaccard_score(input_file)
+    write_output(test_jaccard_smlrt, output_file)
+    
+if __name__ == "__main__":
+    main()
+   
+        
+        
+        
+"""
         # Try different similarity metrics
         bray_curtis_dissimilarity = distance.braycurtis(set1, set2)
         #jaccard_distance = distance.jaccard(set1, set2) # no sense to calculate this one, doesn't take into account the frequence [counts]
@@ -54,6 +83,9 @@ for i in range(len(sample_ids)):
         tb_coefficient, p_value_tb = kendalltau(set1, set2)
         pearson_coefficient, p_value_pearson = pearsonr(set1,set2)
         rho_coefficient, p_value_spearman = spearmanr(set1, set2)
+"""
+
+"""
         # Print statements
         print("Bray-Curtis Dissimilarity between {}, {} = {}".format(sample_ids[i], sample_ids[j], bray_curtis_dissimilarity))
         #print("Jaccard Similarity Score between {}, {} = {}.".format(sample_ids[i], sample_ids[j], jaccard_distance))
@@ -64,7 +96,4 @@ for i in range(len(sample_ids)):
         print("Kendall's tau-b correlation coefficient between {}, {} = {} and p-value = {}".format(sample_ids[i], sample_ids[j], tb_coefficient, p_value_tb))
         print("Pearson correlation coefficient between {}, {} = {} and p-value = {}".format(sample_ids[i], sample_ids[j], pearson_coefficient, p_value_pearson))
         print("Spearman correlation coefficient between {}, {} = {} and p-value = {}".format(sample_ids[i], sample_ids[j], rho_coefficient, p_value_spearman))
-
-#print(set1, set2)
-
-# TODO check [unifraq - suitable for sequence data] + add SNB score
+"""
