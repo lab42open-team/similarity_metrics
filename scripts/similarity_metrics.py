update and correct script - jaccard score calculation

scripts/similarity_metrics.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.5
+
 ### This is a script to calculate similarity metrics between studies and samples in a study. 
 # All vs All:
 # 1. Sample vs Sample within a study 
@@ -5,23 +7,30 @@
 # 3. For v4.0 and v5.0: Samples vs Samples in different studies of the same LSU or SSU type
 # 4. Studies vs Studies independently from version or type 
 
+# script name: similarity_metrics.py
+# developed by: Nefeli Venetsianou
+# description: calculate similarity metrics between studies and samples in a study
+# input parameters from cmd:
+    # --input_file="input_file_name"
+# framework: CCMRI
+
 import os, sys
 import numpy as np
-# from sklearn.metrics import jaccard_score - works in python > 3
 from scipy.spatial import distance
 from scipy.stats import kendalltau
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
-
-# Set input directory 
-#input_dir = "/ccmri/profile_matching/test_dataset/MGYS_taxa_counts_output" # test
-# Read target file 
-#input_file = "/ccmri/data_similarity_metrics/test_dataset/MGYS_taxa_counts_output/MGYS00000305_ERP001568_taxonomy_abundances_v1.0.tsv_output.tsv" #[file for file in os.listdir(input_dir)]
-#input_file = "/ccmri/similarity_metrics/data/test_dataset/output.tsv" #[file for file in os.listdir(input_dir)]
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
+#import psutil # Uncomment this line if you want to retrieve total CPU usage 
 
 def jaccard_score(input_file):
     # Dictionary to store counts for each sample
     sample_counts = {}
+    # Record start time 
+    start_time = time.time()
+    
     with open(input_file, "r") as file:
         # Read header & sample IDs
         header = file.readline().strip().split("\t")
@@ -49,22 +58,66 @@ def jaccard_score(input_file):
             jaccard_distance = distance.jaccard(set1, set2) 
             jaccard_scores.append((sample_ids[i], sample_ids[j], jaccard_distance))
             #print("Jaccard Similarity Score between {}, {} = {}.".format(sample_ids[i], sample_ids[j], jaccard_distance))
-
+    # Record end time 
+    end_time = time.time()
+    # Calculate execution time 
+    execution_def_time = end_time - start_time
+    logging.info("Execution def time = {} seconds".format(execution_def_time))
     return jaccard_scores
                 
-#jaccard = jaccard_score(input_file)                
-
-def write_output(jaccard_scores, output_file):        
+def write_output(jaccard_scores, input_file, output_dir):
+    # Extract version pipeline from input file name 
+    input_file_name = os.path.basename(input_file)
+    version = input_file_name.split("_super_table.tsv")[0]
+    # Construct output file name and path
+    output_file_name = version + "_jaccard_output.tsv"
+    output_file = os.path.join(output_dir, output_file_name)        
     with open(output_file, "w") as file:
         file.write("Sample i\tSample j\tJaccard Score\n")
         for item in jaccard_scores:
             file.write("{}\t{}\t{}\n".format(item[0], item[1], item[2]))
             
-def main():
-    input_file = "/ccmri/similarity_metrics/data/SuperTable/v5.0_LSU_super_table.tsv"
-    output_file = "/ccmri/similarity_metrics/data/Metrics/v5.0_LSU_jaccard_output.tsv"
-    test_jaccard_smlrt = jaccard_score(input_file)
-    write_output(test_jaccard_smlrt, output_file)
+def main():   
+    # Extract argument values imported from sys.argv
+    arguments_dict = {}
+    for arg in sys.argv[1:]:
+        if '=' in arg:
+            sep = arg.find('=')
+            key, value = arg[:sep], arg[sep + 1:]
+            arguments_dict[key] = value
+    # Set default input - output directories
+    input_dir = "/ccmri/similarity_metrics/data/SuperTable/"
+    output_dir = "/ccmri/similarity_metrics/data/Metrics/"
+           
+    # Update parameters based on the values passed by the command line 
+    input_file = arguments_dict.get("--input_file")
+    if not input_file:
+        input_file = input("Please provide input file name, eg. v5.0_LSU_super_table.tsv: ")
+    if input_file:
+        input_file = os.path.join(input_dir, input_file)
+    else:
+        logging.warning("No input file provided.")
+        sys.exit(1)
+        
+    # Record total start time 
+    tot_start_time = time.time()
+    
+    # Perform jaccard similarity calculation
+    jaccard_smlrt = jaccard_score(input_file)
+    
+    # Write output file
+    write_output(jaccard_smlrt, input_file, output_dir)
+    logging.info("Output written successfully to: {}".format(output_dir))    
+    
+    # Record total end time  
+    tot_end_time = time.time()
+    
+    # Calculate total execution time
+    tot_execution_time = tot_end_time - tot_start_time
+    logging.info("Total execution time is {} seconds".format(tot_execution_time))
+    
+    # Get current CPU usage for 5 seconds - interval time can be adjusted 
+    #logging.info("CPU usage = ", psutil.cpu_percent(interval = 5)) # Uncomment this line if you want to retrieve total CPU usage
     
 if __name__ == "__main__":
     main()