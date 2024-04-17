c8c2530fc0621b9e9aed4f31237c4e0a60c3f080
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-04-17 14:53:44 +0300

    update script

--- scripts/non_ra_taxa_counts.py
+++ scripts/non_ra_taxa_counts.py
@@ -2,7 +2,9 @@
 
 # script name: taxa_counts.py
 # developed by: Nefeli Venetsianou
-# description: get taxa counts per sample in study (Taxonomic hierarchy: k > p > c > o > f  > g > s)
+# description: 
+    # Get taxa counts per sample in study (Taxonomic hierarchy: k > p > c > o > f  > g > s). 
+    # Works for non normalized data (NO relative abundances calculated).
 # input parameters from cmd:
     # --input_dir="your_input_directory"
     # --output_dir="your_output_directory"
@@ -19,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 input_directory = "/ccmri/data/mgnify/frozen_february_2024/2024_1_17/harvested_mgnify_studies"
 #input_directory = "/ccmri/similarity_metrics/data/test_dataset/test_MGYS" #- test 
 #output_directory = "/ccmri/similarity_metrics/data/test_dataset/MGYS_taxa_counts_output" #- test
-output_directory = "/ccmri/similarity_metrics/data/taxa_counts_output"
+output_directory = "/ccmri/similarity_metrics/data/non_ra_taxa_counts_output"
 ignore_prefix = False 
 
 # Extract argument values imported from sys.argv
