b7be8bce721532be78fa720331148f895fe178b0
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-05-16 10:06:00 +0300

    update code: add input filename parameter, adjust plot title and output file name according to input filename (extract version)

--- scripts/taxonomy_counts.py
+++ scripts/taxonomy_counts.py
@@ -4,16 +4,31 @@
 # developed by: Nefeli Venetsianou
 # description: 
     # Calculate counts per taxonomy (k > p > c > o > f > g > s) in each version Super Table. 
-# no input parameters are allowed 
+# Input Parameters:
+    # filename: add the file name of interest
+    # usage: ./taxonomy_counts.py lf_v4.1_LSU_super_table.tsv
 # framework: CCMRI
-# last update: 14/05/2024
+# last update: 16/05/2024
 
 import pandas as pd 
 import re
+import sys, os
 from matplotlib import pyplot as plt
 
+parent_directory = "/ccmri/similarity_metrics/data/super_table/long_format/"
+# Add filename from command-line arguments
+if len(sys.argv) < 2:
+    print("Please provide the filename (eg. lf_v4.1_LSU_super_table.tsv)")
+    sys.exit(1)   
+filename = sys.argv[1]
+file_path = os.path.join(parent_directory, filename)
+# check is file exists and is a tsv file
+if not os.path.exists(file_path) or not file_path.endswith(".tsv"):
+    print("File not found or not a .tsv file. Please provide a valid file.")
+    sys.exit(1)
+
 # Read file 
-df = pd.read_csv("/ccmri/similarity_metrics/data/super_table/long_format/lf_v1.0_super_table.tsv", sep = "\t")
+df = pd.read_csv(file_path, sep = "\t")
 # Extract taxa column
 taxa = df["Taxa"]
 #print(taxa)
@@ -49,10 +64,15 @@ non_zero_counts = {level : count for level, count in unique_counts.items() if co
 # Combine taxonomic labels and counts for display
 labels_with_counts = ["{}:{} ({}%)".format(level, count, percentages[level]) for level, count in non_zero_counts.items()]
 plt.pie(non_zero_counts.values(), labels = labels_with_counts)
-plt.title("Unique Taxa Counts v1.0")
-plt.savefig("/ccmri/similarity_metrics/data/super_table/long_format/plots/v1.0_taxonomy.png")
+# Adjusting title name by filename provided
+plot_title = "Unique Taxa Counts " + re.sub(r"^lf_", "", filename)[:-16] 
+plt.title(plot_title)
+# Adjusting output filename according to input filename 
+output_direcroty = "/ccmri/similarity_metrics/data/super_table/long_format/plots/"
+output_filename = re.sub(r"^lf_", "", filename)[:-16] + "_taxonomy_counts.png"
+output_file_path = os.path.join(output_direcroty, output_filename)
+plt.savefig(output_file_path)
 
 # Print counts of unique taxa found
 for level, count in unique_counts.items():
     print("{}:{}".format(level, count))
-        
