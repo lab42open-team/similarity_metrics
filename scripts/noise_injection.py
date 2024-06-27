ecac32bc55a5eea8943f59ae8fe433a496f75c83
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-06-27 15:39:33 +0300

    reconstruct code - update main and injection functions

--- scripts/noise_injection.py
+++ scripts/noise_injection.py
@@ -5,8 +5,9 @@
 # description: 
     # Noise injection to the downsampled data, in order to create noisy versions. 
     # Gaussian Noise Injection is selected, at different levels of noise, by adjusting standard deviation.  
+    # Impulse Noise Injection increases/decreases counts per sample, in order to randomly add noise. Noise level van be adjusted by changing the probability of injecting noise.
 # framework: CCMRI
-# last update: 26/06/2024
+# last update: 27/06/2024
 
 import os
 import pandas as pd
@@ -15,15 +16,14 @@ import matplotlib.pyplot as plt
 import logging
 logging.basicConfig(level=logging.INFO)
 
-def gaussian_noise_injection(input_file, output_dir):
+def gaussian_noise_injection(input_file, original_plot_file, output_dir, gaussian_std_dev_noise):
     df = pd.read_csv(input_file, sep="\t")
+    # Read original RA file - for plotting reasons only 
+    original_plot_df = pd.read_csv(original_plot_file,sep="\t")
     # Apply Gaussian noise injection
     # Mean is set to 0 in order to create noise centered around the original values. 
     mean_noise = 0
-    # To change the intensity (level) of noise, adjust the standard deviation.
-    # Higher std_dev_noise leads to noisier dataset, while lower std_dev_noise leads to a dataset closer to the original. 
-    std_dev_noise = 5
-    noise = np.random.normal(mean_noise, std_dev_noise, len(df["Count"]))
+    noise = np.random.normal(mean_noise, gaussian_std_dev_noise, len(df["Count"]))
     # Create a copy of df
     noisy_counts_df = df.copy()
     # Add noise to the "Count" column 
@@ -37,19 +37,17 @@ def gaussian_noise_injection(input_file, output_dir):
     # Save noisy version
     # Extract filename from input file 
     file_name = os.path.basename(input_file)
-    # Remove "ra_" from file_name
-    filename = file_name[3:]
     # Construct output file_path 
-    output_file = os.path.join(output_dir, "noisy_{}_stdDev_".format(std_dev_noise) + filename)
+    output_file = os.path.join(output_dir, "noisy_{}_stdDev_".format(gaussian_std_dev_noise) + file_name)
     # Save noisy version 
     noisy_counts_df.to_csv(output_file, sep="\t", index=False)    
-    logging.info("Noisy version of {} level, saved to: {}".format(std_dev_noise, output_file))
+    logging.info("Noisy version of {} level, saved to: {}".format(gaussian_std_dev_noise, output_file))
     
     # Plotting Data
     plt.figure(figsize=(15,5))
     # Plot original data
     plt.subplot(1,3,1)
-    plt.hist(df["Count"], bins=50, alpha=0.7, label="Original Count")
+    plt.hist(original_plot_df["Count"], bins=50, alpha=0.7, label="Original Count")
     plt.title("Original Data Plot")
     plt.xlabel("Count")
     plt.ylabel("Frequency")
@@ -74,19 +72,19 @@ def gaussian_noise_injection(input_file, output_dir):
     noisy_plot_path = os.path.join(output_dir, "noisy_data_plot.png")
     # Save full figure
     # Remove .tsv suffix
-    out_filename = filename[:-4]
+    out_filename = file_name[:-4]
     # Construct path 
-    img_plot_path = os.path.join(output_dir, "histograms_n{}_stdDev_".format(std_dev_noise) + out_filename + ".png")
+    img_plot_path = os.path.join(output_dir, "histograms_n{}_stdDev_".format(gaussian_std_dev_noise) + out_filename + ".png")
     plt.savefig(img_plot_path)
     logging.info("Plot saved to: {}".format(img_plot_path))
     
-def impulse_noise_injection(input_file, output_dir):
+def impulse_noise_injection(input_file, original_plot_file, output_dir, impulse_noise_level):
     # Read input file
     df = pd.read_csv(input_file, sep="\t")
+    # Read original RA file - for plotting reasons only 
+    original_plot_df = pd.read_csv(original_plot_file, sep="\t")
     # Create a copy of DataFrame
     noisy_counts_df = df.copy()
-    # Probability of injecting impulse noise to counts - set to 1 to apply for all counts
-    prob_impulse = 1
     # Lower bound value for impulse noise 
     low_val = 0
     # Higher bound value for impulse noise
@@ -94,7 +92,7 @@ def impulse_noise_injection(input_file, output_dir):
     # Generate random impulse noise values # p represents the probability of choosing each low-high val
     random_values = np.random.choice([low_val, high_val], size=len(df["Count"]), p=[0.9,0.1])
     # Generate a mask fro where to apply the impulse noise
-    mask = np.random.choice([True, False], size=len(df["Count"]), p=[1-prob_impulse, prob_impulse])
+    mask = np.random.choice([True, False], size=len(df["Count"]), p=[impulse_noise_level, 1 - impulse_noise_level])
     # Apply the impulse noise to the Count column
     noisy_counts_df.loc[mask, "Count"] = random_values[mask]
     # Normalize counts (sum to 1)
@@ -104,32 +102,30 @@ def impulse_noise_injection(input_file, output_dir):
     # Save noisy version
     # Extract filename from input file 
     file_name = os.path.basename(input_file)
-    # Remove "ra_" from file_name
-    filename = file_name[3:]
     # Construct output file_path 
-    output_file = os.path.join(output_dir, "noisy_impulse_" + filename)
+    output_file = os.path.join(output_dir, "noisy_{}_impL_{}".format(impulse_noise_level, file_name))
     # Save noisy version 
     noisy_counts_df.to_csv(output_file, sep="\t", index=False)    
-    logging.info("Impulse noisy version saved to: {}".format(output_file))
+    logging.info("Impulse noisy version of level {} saved to: {}".format(impulse_noise_level, output_file))
     
     # Plotting Data
     plt.figure(figsize=(15,5))
     # Plot original data
     plt.subplot(1,3,1)
-    plt.hist(df["Count"], bins=50, alpha=0.7, label="Original Count")
+    plt.hist(original_plot_df["Count"], bins=50, alpha=0.7, label="Original Count")
     plt.title("Original Data Plot")
     plt.xlabel("Count")
     plt.ylabel("Frequency")
     # Plot Gaussian noise
     plt.subplot(1,3,2)
-    plt.hist(random_values, bins=50, alpha=0.7, label="Impulse noise")
-    plt.title("Gaussian Noise Plot")
-    plt.xlabel("Gaussian noise")
+    plt.hist(random_values, bins=50, alpha=0.7, label="Impulse Noise")
+    plt.title("Impulse Noise Plot")
+    plt.xlabel("Impulse noise")
     plt.ylabel("Frequency")
     
     # Plot noisy data
     plt.subplot(1,3,3)
-    plt.hist(noisy_counts_df["Count"], bins=50, alpha=0.7, label="Noisy count")
+    plt.hist(noisy_counts_df["Count"], bins=50, alpha=0.7, label="Noisy Count")
     plt.title("Noisy Data Plot")
     plt.xlabel("Count")
     plt.ylabel("Frequency")
@@ -141,21 +137,27 @@ def impulse_noise_injection(input_file, output_dir):
     noisy_plot_path = os.path.join(output_dir, "noisy_data_plot.png")
     # Save full figure
     # Remove .tsv suffix
-    out_filename = filename[:-4]
+    out_filename = file_name[:-4]
     # Construct path 
-    img_plot_path = os.path.join(output_dir, "histograms_impulse_" + out_filename + ".png")
+    img_plot_path = os.path.join(output_dir, "histograms_{}_impL_".format(impulse_noise_level) + out_filename + ".png")
     plt.savefig(img_plot_path)
     logging.info("Plot saved to: {}".format(img_plot_path))
     
 def main():
-    input_file = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/downsampled_data/1000_samples/normalized/ra_v4.1_LSU_ge_filtered.tsv"
+    input_file = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/downsampled_data/1000_samples/d_v5.0_LSU_ge_filtered.tsv"
+    original_plot_file = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/downsampled_data/1000_samples/normalized/ra_d_v5.0_LSU_ge_filtered.tsv"
     # output to be defined according to std_noise_level picked
-    gaussian_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/gaussian_noisy_data/d_1000/std_noise_level_5"
-    impulse_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/impulse_noisy_data"
+    gaussian_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/gaussian_noisy_data/d_1000/0.2_stdDev"
+    impulse_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/impulse_noisy_data/d_1000/0.2_noiseLevel"
+    # Define Noise Level per noise injection
+    # To change the intensity of noise, adjust the standard deviation. - Noise Level
+    # Higher std_dev_noise leads to noisier dataset, while lower std_dev_noise leads to a dataset closer to the original. 
+    gaussian_std_dev_noise = 0.2
+    # Probability of injecting impulse noise to counts - Noise Level
+    impulse_noise_level = 0.2
     # Apply Gaussian injection noise
-    #gaussian_noise_injection(input_file, gaussian_output_dir)    
-    impulse_noise_injection(input_file, impulse_output_dir)
+    gaussian_noise_injection(input_file, original_plot_file, gaussian_output_dir, gaussian_std_dev_noise)    
+    impulse_noise_injection(input_file, original_plot_file, impulse_output_dir, impulse_noise_level)
     
 if __name__ == "__main__":
     main()
-    
