943aad2d9c1cf0be286602c090c1ea72cf9a2662
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-07-02 16:34:38 +0300

    update script - correct calculate_similarity_metrics and load_data functions

--- scripts/similarity_metrics.py
+++ scripts/similarity_metrics.py
@@ -8,7 +8,7 @@
     # No input parameters provided, but input/output should be adjusted accordingly. 
         # Please check the session: ### TO BE ADJUSTED ### within the main function. 
 # framework: CCMRI
-# last update: 27/06/2024
+# last update: 02/07/2024
 
 import os
 import pandas as pd
@@ -20,43 +20,57 @@ from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
 import logging
 logging.basicConfig(level=logging.INFO)
 
+
 def load_data(initial_file, noisy_file):
     # Create DataFrames from input files
-    df1 = pd.read_csv(initial_file,sep="\t")
-    df2 = pd.read_csv(noisy_file, sep="\t")
+    df_initial = pd.read_csv(initial_file,sep="\t")
+    df_noisy = pd.read_csv(noisy_file, sep="\t")
     # DEBUGGING LOGS 
-    logging.info("Initial data loaded correctly: {}".format(df1.shape))
-    logging.info("Noisy data loaded correctly: {}".format(df2.shape))
-    # Combine the data into a single DataFrame
-    combined_df = pd.concat([df1, df2])
-    # Pivot table to have taxa as rows and samples as columns 
-    pivot_df = combined_df.pivot_table(index="Taxa", columns="Sample", values="Count", fill_value=0)
-    # DEBUGGING LOGS
-    logging.info("Pivot table shape: {}".format(pivot_df.shape))
-    return df1, df2, pivot_df
+    logging.info("Initial data of {} loaded correctly: {}".format(initial_file, df_initial.shape))
+    logging.info("Noisy data of {} loaded correctly: {}".format(noisy_file, df_noisy.shape))
+    return df_initial, df_noisy
 
-def calculate_similarity_scores(df1, df2, pivot_df, output_dir, noisy_file_name, metric):
-    similarity_pairs = []
-    for sample1 in df1["Sample"].unique():
-        for sample2 in df2["Sample"].unique():
-            # Extract vectors for the samples
-            vector1 = pivot_df[sample1].values.reshape(1, -1)
-            vector2 = pivot_df[sample2].values.reshape(1, -1)
-            if metric == "euclidean":
-                # Calculate Euclidean distance and similarity score
-                euclidean_dist = euclidean_distances(vector1, vector2)[0][0]
-                similarity_score = 1 / (1 + euclidean_dist)
-            elif metric == "cosine":
-                # Compute Cosine distance and similarity score
-                cosine_distance = cosine_distances(vector1, vector2)[0][0]
-                similarity_score = 1 - cosine_distance
+def calculate_similarity_scores(df_initial, df_noisy, output_dir, noisy_file_name, metric):
+    # Initialize dictionary to store similarity scores
+    similarity_scores = []   
+    # Iterate over each sample in original and noisy version
+    for sample1 in df_initial["Sample"].unique():
+        for sample2 in df_noisy["Sample"].unique():
+            # Create DataFrames with the current sample values
+            df_initial_sample = df_initial[df_initial["Sample"] == sample1]
+            df_noisy_sample = df_noisy[df_noisy["Sample"] == sample2]
+            # DEBUGGING LOGS
+            logging.info("Initial Sample Counts Shape: {}".format(df_initial_sample.shape))
+            logging.info("Noisy Sample Counts Shape: {}".format(df_noisy_sample.shape))
+            # Merge on Taxa to align counts
+            merged_df = pd.merge(df_initial_sample[["Taxa", "Count"]], df_noisy_sample[["Taxa", "Count"]], 
+                                 on="Taxa", suffixes = ("_initial", "_noisy"))
+            # Check if merged DataFrame is not empty
+            if not merged_df.empty:
+                # Extract aligned counts 
+                initial_sample_counts = merged_df["Count_initial"].values  
+                noisy_sample_counts = merged_df["Count_noisy"].values
+                # DEBUGGING LOGS
+                logging.info("Initial Sample Counts: {}".format(initial_sample_counts.shape))
+                logging.info("Noisy Sample Counts: {}".format(noisy_sample_counts.shape))
+                # Create numpy array and reshape to a 2-dimensional array
+                sample1_np = np.array(initial_sample_counts).reshape(1, -1)
+                sample2_np = np.array(noisy_sample_counts).reshape(1, -1)
+                if metric == "euclidean":
+                    euclidean_dist = euclidean_distances(sample1_np, sample2_np)[0][0]
+                    similarity_score = 1 / (1 + euclidean_dist)
+                elif metric == "cosine":
+                    cosine_dist = cosine_distances(sample1_np, sample2_np)[0][0]
+                    similarity_score = 1 - cosine_dist
+                else:
+                    raise ValueError("Unsupported metric: {}".format(metric))
+            # If merged DataFrame is empty, assign default similarity score as zero.
             else:
-                raise ValueError("Unsupported metric: {}".format(metric))
-            
-            # Append to similarity pairs list 
-            similarity_pairs.append([sample1, sample2, similarity_score])
+                similarity_score = 0
+            # Store similarity scores to dictionary
+            similarity_scores.append([sample1, sample2, similarity_score])
     # Convert to DataFrame
-    similarity_df  = pd.DataFrame(similarity_pairs, columns=["Initial_Sample", "Noisy_Sample", "Similarity_Score"])
+    similarity_df  = pd.DataFrame(similarity_scores, columns=["Initial_Sample", "Noisy_Sample", "Similarity_Score"])
     # DEBUGGING LOGS
     logging.info("Sample {} similarity scores:\n{}".format(metric, similarity_df.head()))
     # Group by Noisy Sample and sort according to similarity score within each group
@@ -79,17 +93,17 @@ def main():
     # Impulse Noise Parent dir
     impulse_noisy_parent_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/impulse_noisy_data/d_1000/0.9_noiseLevel"
     # Define file names to be compared
-    intial_file_name = "ra_d_v4.1_LSU_ge_filtered.tsv"
+    initial_file_name = "ra_d_v4.1_LSU_ge_filtered.tsv"
     gaussian_noisy_file_name = "noisy_5_stdDev_d_v4.1_LSU_ge_filtered.tsv"
     impulse_noisy_file_name = "noisy_0.9_impL_d_v4.1_LSU_ge_filtered.tsv"
-    initial_file = os.path.join(initial_parent_directory, intial_file_name)
+    initial_file = os.path.join(initial_parent_directory, initial_file_name)
     ### --- ###
     
     # Load noisy data 
     gaussian_noisy_file = os.path.join(gaussian_noisy_parent_directory, gaussian_noisy_file_name)
-    gaussian_df1, gaussian_df2, gaussian_pivot_df = load_data(initial_file, gaussian_noisy_file)
+    gaussian_df1, gaussian_df2 = load_data(initial_file, gaussian_noisy_file)
     impulse_noisy_file = os.path.join(impulse_noisy_parent_directory, impulse_noisy_file_name)
-    impulse_df1, impulse_df2, impulse_pivot_df = load_data(initial_file, impulse_noisy_file)
+    impulse_df1, impulse_df2 = load_data(initial_file, impulse_noisy_file)
     # Define Gaussian Output dir
     gaussian_euclidean_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/similarity_metrics/sim_initialVSgaussian_noisy/euclidean_output/"
     gaussian_cosine_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/similarity_metrics/sim_initialVSgaussian_noisy/cosine_output"
@@ -98,67 +112,15 @@ def main():
     impulse_cosine_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/similarity_metrics/sim_initialVSimpulse_noisy/cosine_output"
     # Calculate and Save similarities 
     # Gaussian noisy files 
-    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_pivot_df, gaussian_euclidean_output_dir, gaussian_noisy_file, metric="euclidean")
+    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_euclidean_output_dir, gaussian_noisy_file, metric="euclidean")
     logging.info("Euclidean similarity scores for initial VS gaussian noisy files calculated successfully.")
-    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_pivot_df, gaussian_cosine_output_dir, gaussian_noisy_file, metric="cosine")
+    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_cosine_output_dir, gaussian_noisy_file, metric="cosine")
     logging.info("Cosine similarity scores for initial VS gaussian noisy files calculated successfully.")
     # Impulse noisy files
-    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_pivot_df, impulse_euclidean_output_dir, impulse_noisy_file, metric="euclidean")
+    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_euclidean_output_dir, impulse_noisy_file, metric="euclidean")
     logging.info("Euclidean similarity scores for initial VS impulse noisy files calculated successfully.")
-    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_pivot_df, impulse_cosine_output_dir, impulse_noisy_file, metric="cosine")
+    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_cosine_output_dir, impulse_noisy_file, metric="cosine")
     logging.info("Cosine similarity scores for initial VS impulse noisy files calculated successfully.")
 
 if __name__ == "__main__":
     main()
-    
-    
-    """
-    ### FIRST WAY | TRIAL ###
-    def calculate_euclidean_scores(df1, df2, pivot_df, euclidean_output_dir, noisy_file_name):
-    euclidean_similarity_pairs = []
-    for sample1 in df1["Sample"].unique():
-        for sample2 in df2["Sample"].unique():
-            # Compute the Euclidean distance matrix between Sample1 and Sample2
-            euclidean_dist = euclidean(pivot_df[sample1], pivot_df[sample2])
-            # Calculate similarity score
-            euclidean_similarity_score = 1 / (1 + euclidean_dist)
-            # Append to euclidean similarity pairs list 
-            euclidean_similarity_pairs.append([sample1, sample2, euclidean_similarity_score])
-    # Convert to DataFrame
-    euclidean_similarity_df = pd.DataFrame(euclidean_similarity_pairs, columns=["Initial_Sample", "Noisy_Sample", "Similarity_Score"])
-    # DEBUGGING LOGS
-    logging.info("Sample Euclidean similarity scores:\n{}".format(euclidean_similarity_df.head()))
-    # Group by Noisy Sample and sort according to similarity score within each group
-    grouped_euclidean_similarity_df = euclidean_similarity_df.groupby("Noisy_Sample").apply(lambda x: x.sort_values(by="Similarity_Score", ascending=False))
-    # Define euclidean output directory and path
-    euclidean_output_path = os.path.join(euclidean_output_dir, "e_initial_VS_{}".format(os.path.basename(noisy_file_name)))
-    # Save DataFrame
-    grouped_euclidean_similarity_df.to_csv(euclidean_output_path, sep="\t", index=False)
-    logging.info("Euclidean output saved to: {}".format(euclidean_output_path))
-    return grouped_euclidean_similarity_df
-    
-def calculate_cosine_scores(df1, df2, pivot_df, cosine_output_dir, noisy_file_name):
-    cosine_similarity_pairs = []
-    for sample1 in df1["Sample"].unique():
-        for sample2 in df2["Sample"].unique():
-            # Compute the Cosine distance between Sample1 and Sample2
-            cosine_dist = distance.cosine(pivot_df[sample1], pivot_df[sample2])
-            # Calculate cosine score
-            cosine_similarity_score = 1 - cosine_dist
-            # Append to cosine similarity pairs list
-            cosine_similarity_pairs.append([sample1, sample2, cosine_similarity_score])
-    # Convert to DataFrame
-    cosine_similarity_df = pd.DataFrame(cosine_similarity_pairs, columns=["Initial_Sample", "Noisy_Sample", "Similarity_Score"])
-    # DEBUGGING LOGS
-    logging.info("Sample Cosine similarity scores:\n{}".format(cosine_similarity_df.head()))
-    # Group by Noisy Sample and sort according to similarity score within each group
-    grouped_cosine_similarity_df = cosine_similarity_df.groupby("Noisy_Sample").apply(lambda x: x.sort_values(by="Similarity_Score", ascending=False))
-    # Define cosine output directory and path 
-
-    cosine_output_path = os.path.join(cosine_output_dir, "c_initial_VS_{}".format(os.path.basename(noisy_file_name)))
-    # Save DataFrame 
-    grouped_cosine_similarity_df.to_csv(cosine_output_path, sep="\t", index=False)
-    logging.info("Cosine output saved to: {}".format(cosine_output_path))
-    return grouped_cosine_similarity_df
-
-    """
