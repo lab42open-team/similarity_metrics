48dd4a1a440773f6a938102e4ac3c8e73a3ec043
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-08-08 18:04:23 +0300

    output directory correction

--- scripts/subset_creation.py
+++ scripts/subset_creation.py
@@ -16,7 +16,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 input_file = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/initial_data/v5.0_SSU_ge_filtered.tsv"
-output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/initial_data/downsampled_data"
+output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/initial_data/down_filtered_data"
 # output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/downsampled_data/1000_samples" # For noise injection
 
 def main():
