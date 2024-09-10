cfba0be31c612388c4a5adf8ae03053fd348d3a5
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-09-10 16:51:12 +0300

    Jensen-shannon divergence added

--- scripts/sim_metrics_originalVSnoisy.py
+++ scripts/sim_metrics_originalVSnoisy.py
@@ -15,6 +15,7 @@ import pandas as pd
 import numpy as np
 from sklearn import metrics
 from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
+from scipy.spatial.distance import jensenshannon
 #from scipy.spatial import distance 
 #from scipy.spatial.distance import euclidean, pdist, squareform
 import logging
@@ -39,8 +40,8 @@ def calculate_similarity_scores(df_initial, df_noisy, output_dir, noisy_file_nam
             df_initial_sample = df_initial[df_initial["Sample"] == sample1]
             df_noisy_sample = df_noisy[df_noisy["Sample"] == sample2]
             # DEBUGGING LOGS
-            logging.info("Initial Sample Counts Shape: {}".format(df_initial_sample.shape))
-            logging.info("Noisy Sample Counts Shape: {}".format(df_noisy_sample.shape))
+            #logging.info("Initial Sample Counts Shape: {}".format(df_initial_sample.shape))
+            #logging.info("Noisy Sample Counts Shape: {}".format(df_noisy_sample.shape))
             # Merge on Taxa to align counts
             merged_df = pd.merge(df_initial_sample[["Taxa", "Count"]], df_noisy_sample[["Taxa", "Count"]], 
                                  on="Taxa", suffixes = ("_initial", "_noisy"))
@@ -50,8 +51,8 @@ def calculate_similarity_scores(df_initial, df_noisy, output_dir, noisy_file_nam
                 initial_sample_counts = merged_df["Count_initial"].values  
                 noisy_sample_counts = merged_df["Count_noisy"].values
                 # DEBUGGING LOGS
-                logging.info("Initial Sample Counts: {}".format(initial_sample_counts.shape))
-                logging.info("Noisy Sample Counts: {}".format(noisy_sample_counts.shape))
+                #logging.info("Initial Sample Counts: {}".format(initial_sample_counts.shape))
+                #logging.info("Noisy Sample Counts: {}".format(noisy_sample_counts.shape))
                 # Create numpy array and reshape to a 2-dimensional array
                 sample1_np = np.array(initial_sample_counts).reshape(1, -1)
                 sample2_np = np.array(noisy_sample_counts).reshape(1, -1)
@@ -61,6 +62,15 @@ def calculate_similarity_scores(df_initial, df_noisy, output_dir, noisy_file_nam
                 elif metric == "cosine":
                     cosine_dist = cosine_distances(sample1_np, sample2_np)[0][0]
                     similarity_score = 1 - cosine_dist
+                elif metric == "jsd":
+                    # Add a small epsilon to avoid zeros
+                    epsilon = 1e-10
+                    # Normalize counts to probability distributions
+                    sample1_probs = (sample1_np + epsilon) / np.sum(sample1_np + epsilon)
+                    sample2_probs = (sample2_np + epsilon) / np.sum(sample2_np + epsilon)
+                    # Calculate Jensen-Shannon divergence
+                    distance = jensenshannon(sample1_probs[0], sample2_probs[0]) # scipy's jensenshannon function returns the square root of JSD
+                    similarity_score = 1 - distance  # Convert to similarity
                 else:
                     raise ValueError("Unsupported metric: {}".format(metric))
             # If merged DataFrame is empty, assign default similarity score as zero.
@@ -83,43 +93,33 @@ def calculate_similarity_scores(df_initial, df_noisy, output_dir, noisy_file_nam
     
 def main():
     # Define parent directory of intial data [downsampled]
-    initial_parent_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noisy_versions/downsampled_data/1000_samples/normalized/"
-    
+    initial_parent_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/noisy_versions/d_1000_data/normalized"   
     ### TO BE ADJUSTED ###
-    # Define parent directory of noisy data [type and level of noise] 
-    # Gaussian Noise Parent dir
-    gaussian_noisy_parent_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/noisy_versions/gaussian_noisy_data/d_1000/5_stdDev"
-    # Impulse Noise Parent dir
-    impulse_noisy_parent_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/noisy_versions/impulse_noisy_data/d_1000/0.9_noiseLevel"
-    # Define file names to be compared
-    initial_file_name = "ra_d_v4.1_LSU_ge_filtered.tsv"
-    gaussian_noisy_file_name = "noisy_5_stdDev_d_v4.1_LSU_ge_filtered.tsv"
-    impulse_noisy_file_name = "noisy_0.9_impL_d_v4.1_LSU_ge_filtered.tsv"
-    initial_file = os.path.join(initial_parent_directory, initial_file_name)
+    # Initial files
+    initial_file_name = "ra_d_v5.0_LSU_ge_filtered.tsv"
+    initial_file = os.path.join(initial_parent_directory, initial_file_name)   
+    # Noisy files
+    downsampled_noisy_parent_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/noisy_versions/downsampled_noisy_version"
+    downsampled_noise_version_directory = os.path.join(downsampled_noisy_parent_dir, "0.25_ratio")
+    downsampled_file_name = "downsampled_0.25_ratio_d_v5.0_LSU_ge_filtered.tsv"  
     ### --- ###
     
     # Load noisy data 
-    gaussian_noisy_file = os.path.join(gaussian_noisy_parent_directory, gaussian_noisy_file_name)
-    gaussian_df1, gaussian_df2 = load_data(initial_file, gaussian_noisy_file)
-    impulse_noisy_file = os.path.join(impulse_noisy_parent_directory, impulse_noisy_file_name)
-    impulse_df1, impulse_df2 = load_data(initial_file, impulse_noisy_file)
-    # Define Gaussian Output dir
-    gaussian_euclidean_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSgaussian_noisy/euclidean_output/"
-    gaussian_cosine_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSgaussian_noisy/cosine_output"
-    # Define Impulse Output dir
-    impulse_euclidean_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSimpulse_noisy/euclidean_output"
-    impulse_cosine_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSimpulse_noisy/cosine_output"
+    downsampled_noisy_file = os.path.join(downsampled_noise_version_directory, downsampled_file_name)
+    downsampled_df1, downsampled_df2 = load_data(initial_file, downsampled_noisy_file)
+
+    # Define Downsampled Output dir
+    downsampled_euclidean_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSgaussian_noisy/euclidean_output/"
+    downsampled_cosine_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSgaussian_noisy/cosine_output"
+    downsampled_jensen_shannon_output_dir = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/noise_injection/similarity_metrics/sim_initialVSdownsampled_noisy/jensen_shannon_output"
+    
     # Calculate and Save similarities 
-    # Gaussian noisy files 
-    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_euclidean_output_dir, gaussian_noisy_file, metric="euclidean")
-    logging.info("Euclidean similarity scores for initial VS gaussian noisy files calculated successfully.")
-    calculate_similarity_scores(gaussian_df1, gaussian_df2, gaussian_cosine_output_dir, gaussian_noisy_file, metric="cosine")
-    logging.info("Cosine similarity scores for initial VS gaussian noisy files calculated successfully.")
-    # Impulse noisy files
-    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_euclidean_output_dir, impulse_noisy_file, metric="euclidean")
-    logging.info("Euclidean similarity scores for initial VS impulse noisy files calculated successfully.")
-    calculate_similarity_scores(impulse_df1, impulse_df2, impulse_cosine_output_dir, impulse_noisy_file, metric="cosine")
-    logging.info("Cosine similarity scores for initial VS impulse noisy files calculated successfully.")
+    #calculate_similarity_scores(downsampled_df1, downsampled_df2, downsampled_euclidean_output_dir, downsampled_noisy_file, metric="euclidean")
+    #logging.info("Euclidean similarity scores for initial VS downsampled noisy files calculated successfully.")
+    #calculate_similarity_scores(downsampled_df1, downsampled_df2, downsampled_cosine_output_dir, downsampled_noisy_file, metric="cosine")
+    #logging.info("Cosine similarity scores for initial VS downsampled noisy files calculated successfully.")
+    calculate_similarity_scores(downsampled_df1, downsampled_df2, downsampled_jensen_shannon_output_dir, downsampled_noisy_file, metric="jsd")
+    logging.info("Jensen-Shannon Divergence similarity scores for initial VS downsampled noisy files calculated successfully.")
 
 if __name__ == "__main__":
     main()
