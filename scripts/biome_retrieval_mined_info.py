52f61d039ac35e55238ed75753014baef2ecd369
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2024-10-18 12:47:30 +0300

    delete sample_id information

--- scripts/biome_retrieval_mined_info.py
+++ scripts/biome_retrieval_mined_info.py
@@ -29,33 +29,34 @@ def parse_study_data(file_path):
                 study_id = line.split("\t")[1]
             if line.startswith("biome_info"):
                 biome_info = line.split("\t")[1]
-            if "sample_id" in line:
-                sample_id = line.split("\t")[1]
-                samples.append(sample_id)
-    return study_id, samples, biome_info
+    return study_id, biome_info
 
 def process_multiple_files(input_folder, output_file):
-    # Open output file and write header
-    with open(output_file, "w") as file: 
-        file.write("study_id\tsample_id\tbiome_info\n")
-        # Loop through all files in input folder
-        for root, dirs, files in os.walk(input_folder):
-            # Check if "COMPLETED" file exists and iterate over each directory where it exists
-            if "COMPLETED" in files:
-                for filename in files:
-                    # Process only files named "mined_info.txt"
-                    if filename.endswith(".txt"):
-                    #if "mined_info" in filename:
-                        file_path = os.path.join(root, filename)
-                        # Parse each "mined_info.txt" file
-                        study_id, samples, biome_info = parse_study_data(file_path)
-                        # Write each sample along with the study_id and viome_info
-                        for sample_id in samples:
-                            file.write("{}\t{}\t{}\n".format(study_id, sample_id, biome_info))
-    logging.info("Output saved successfully to: {}".format(output_file))
+    # Initialize set to store unique (study_id, biome_info) pairs
+    study_biome_info = set()
+    
+    # Loop through all files in input folder
+    for root, dirs, files in os.walk(input_folder):
+        # Check if "COMPLETED" file exists and iterate over each directory where it exists
+        if "COMPLETED" in files:
+            for filename in files:
+                # Process only files named "mined_info.txt"
+                if filename.endswith(".txt"):
+                    file_path = os.path.join(root, filename)
+                    # Parse each "mined_info.txt" file
+                    study_id, biome_info = parse_study_data(file_path)
+                    # Add the study_id and biome_info as a tuple to the set
+                    if study_id and biome_info:
+                        study_biome_info.add((study_id, biome_info))
+    
+    # Write the unique (study_id, biome_info) pairs to the output file
+    with open(output_file, "w") as file:
+        file.write("study_id\tbiome_info\n")
+        for study_id, biome_info in study_biome_info:
+            file.write("{}\t{}\n".format(study_id, biome_info))
 
 input_directory = "/ccmri/data/mgnify/frozen_february_2024/2024_1_17/harvested_mgnify_studies"
-output_directory = "/ccmri/similarity_metrics/data/raw_data"
-output_filename = "study_samples_biome_info.tsv"
+output_directory = "/ccmri/similarity_metrics/data/raw_data/lf_raw_super_table/filtered_data/genus/initial_data/"
+output_filename = "study_biome_info.tsv"
 output_file = os.path.join(output_directory, output_filename)
 process_multiple_files(input_directory, output_file)        
