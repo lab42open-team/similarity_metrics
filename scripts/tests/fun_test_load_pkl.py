4357b09569958196f7b4f48e9bc69aaa067f6db3
Nefeli Venetsianou <n.venetsianou@hcmr.gr>
2026-02-09 11:24:42 +0200

    update files organization

--- scripts/tests/fun_test_load_pkl.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-model = joblib.load("/ccmri/similarity_metrics/data/functional/raw_data/GO_abundances/logistic_regression/test_model_v5.0.pkl")
-scaler = joblib.load("/ccmri/similarity_metrics/data/functional/raw_data/GO_abundances/logistic_regression/test_scaler_v5.0.pkl")
-
-print(f"Model: {model}")
-print(f"Scaler: {scaler}")
